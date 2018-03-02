e9277e23222ebaafae2d3ad028f4fb66175c5e7b
dhanashri <dhana.tidke@gmail.com>
2018-03-01 22:18:53 -0800

    Added files textClassifer.py and ra_data_classifier.csv

--- textClassifier.py
+++ textClassifier.py
@@ -1,3 +1,4 @@
+import sys
 import csv
 import nltk
 from sklearn.feature_extraction.text import TfidfVectorizer
@@ -7,37 +8,43 @@ from sklearn.model_selection import GridSearchCV,train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import accuracy_score
 
-data=[]
-with open('ra_data_classifier.csv', 'r', encoding='mac_roman') as csvfile:
-    fileReader = csv.reader(csvfile, delimiter=',', quotechar='"')
-    chunks=[]
-    hasSpace=[]
-    for index,row in enumerate(fileReader):
-        if index == 0:
-            continue
-        data.append([row[1],int(row[2])])
-        chunks.append(row[1])
-        hasSpace.append((row[2]))
+class TextClassification:
 
-def tokenize(text):
-    tokens = nltk.word_tokenize(text)
-    #print(tokens)
-    return tokens
+    def __init__(self, inputChunk):
+        self.chunks=[]
+        self.hasSpace=[]
+        self.inputChunk=inputChunk
 
-train_chunks, test_chunks, train_hasSpace, test_hasSpace =train_test_split(chunks, hasSpace, test_size=0.33)
+    def read(self):
+        with open('ra_data_classifier.csv', 'r', encoding='mac_roman') as csvfile:
+            fileReader = csv.reader(csvfile, delimiter=',', quotechar='"')
+            for index,row in enumerate(fileReader):
+                if index == 0:
+                    continue
+                self.chunks.append(row[1])
+                self.hasSpace.append((row[2]))
 
-steps=[('vectorizer', TfidfVectorizer(tokenizer=tokenize, stop_words='english', sublinear_tf=True)),
-        ('clf', OneVsRestClassifier(MultinomialNB(fit_prior=True, class_prior=None)))]
-pipeline=Pipeline(steps=steps)
-parameters={'vectorizer__min_df': (0, 0.05), 'vectorizer__max_df': (0.3,), 'vectorizer__ngram_range': [(1, 1)], 'clf__estimator__alpha': (1e-2, 1e-3)}
+    def tokenize(self,text):
+        tokens = nltk.word_tokenize(text)
+        return tokens
 
-grid_search_tune=GridSearchCV(pipeline, parameters, cv=2, n_jobs=4, verbose=3)
-grid_search_tune.fit(train_chunks, train_hasSpace)
+    def classify(self):
+        train_chunks, test_chunks, train_hasSpace, test_hasSpace =train_test_split(self.chunks, self.hasSpace, test_size=0.33)
+        steps=[('vectorizer', TfidfVectorizer(tokenizer=self.tokenize, stop_words='english', sublinear_tf=True)),
+                ('clf', OneVsRestClassifier(MultinomialNB(fit_prior=True, class_prior=None)))]
+        pipeline=Pipeline(steps=steps)
+        parameters={'vectorizer__min_df': (0, 0.05), 'vectorizer__max_df': (0.3,), 'vectorizer__ngram_range': [(1, 1)], 'clf__estimator__alpha': (1e-2, 1e-3)}
+        grid_search_tune=GridSearchCV(pipeline, parameters, cv=2, n_jobs=4, verbose=3)
+        grid_search_tune.fit(train_chunks, train_hasSpace)
+        best_clf = grid_search_tune.best_estimator_
 
-best_clf = grid_search_tune.best_estimator_
-string=[test_chunks[1]]
-prediction = best_clf.predict(string)
-print(prediction)
+        predictions = best_clf.predict(test_chunks)
+        print('Accuracy: '+str(accuracy_score(test_hasSpace,predictions)))
 
-predictions = best_clf.predict(test_chunks)
-print(accuracy_score(test_hasSpace,predictions))
+        prediction = best_clf.predict(self.inputChunk)
+        print('Prediction for Input chunk : '+str(inputChunk)+' - '+str(prediction))
+
+inputChunk=sys.argv[1]
+textClassifier=TextClassification([inputChunk])
+textClassifier.read()
+textClassifier.classify()
